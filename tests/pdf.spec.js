1ab8b1aa2bd80bab5607671b6a2092b5697b1e4a
K M Saddam Hossain <dev.saddam@gmail.com>
2026-04-19 12:45:50 +0600

    Invoice Number Spererated

--- tests/pdf.spec.js
+++ tests/pdf.spec.js
@@ -3,8 +3,26 @@ import { test, expect } from '@playwright/test';
 
 import { PDFParse } from 'pdf-parse';
 
-test('Check Specific Email ID in a PDF', async ({ request }) => {
-  const parser = new PDFParse({ url: 'https://kmsaddam.com/resume/Updated_SQA_Saddam_2026.pdf' });
-	const result = await parser.getText();
-  expect(result.text).toContain('dev.saddam@gmaill.com');
+// test('Check Specific Email ID in a Resume(PDF Format)', async ({ request }) => {
+//   const parser = new PDFParse({ url: 'https://kmsaddam.com/resume/Updated_SQA_Saddam_2026.pdf' });
+// 	const result = await parser.getText();
+//   expect(result.text).toContain('dev.saddam@gmaill.com');
+// });
+
+
+test('Check Specific Invoice number in a PDF', async ({ request }) => {
+  const parser = new PDFParse({ url: 'https://slicedinvoices.com/pdf/wordpress-pdf-invoice-plugin-sample.pdf' });
+  // page.waitForResponse()
+  const result = await parser.getText();
+
+  // Extract invoice number in the format 'INV-3337'
+  const invoiceMatch = result.text.match(/INV-\d{4}/);
+  console.log(invoiceMatch[0]);
+  // if (invoiceMatch) {
+  //   const invoiceNumber = invoiceMatch[0];
+  //   console.log('Invoice Number:', invoiceNumber);
+  // } else {
+  //   console.log('Invoice number in format INV-3337 not found.');
+  // }
+expect(invoiceMatch[0]).toContain('INV-3337');
 });
