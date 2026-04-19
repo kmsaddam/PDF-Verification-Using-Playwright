1a3351aea67b20e094823773cc76adf663317aa4
K M Saddam Hossain <dev.saddam@gmail.com>
2026-04-19 11:52:07 +0600

    Changed Test Case

--- tests/pdf.spec.js
+++ tests/pdf.spec.js
@@ -3,16 +3,8 @@ import { test, expect } from '@playwright/test';
 
 import { PDFParse } from 'pdf-parse';
 
-test('Read PDF', async ({ request }) => {
-  // const res = await request.get('https://kmsaddam.com/resume/Updated_SQA_Saddam_2026.pdf');
-  // const buffer = await res.body();
-
-  // const pdfData = await pdfParse(buffer);
-
+test('Check Specific Email ID in a PDF', async ({ request }) => {
   const parser = new PDFParse({ url: 'https://kmsaddam.com/resume/Updated_SQA_Saddam_2026.pdf' });
-
 	const result = await parser.getText();
-	//console.log(result.text);
-
   expect(result.text).toContain('dev.saddam@gmaill.com');
 });
