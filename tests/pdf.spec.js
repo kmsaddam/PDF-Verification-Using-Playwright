9bc84e7e1a1b932bc2dfa72ab4433b0c7d45181d
K M Saddam Hossain <dev.saddam@gmail.com>
2026-04-19 11:50:47 +0600

    PDF Parser package updated

--- tests/pdf.spec.js
+++ tests/pdf.spec.js
@@ -1,37 +1,18 @@
 
-
-// import {pdfParse} from 'pdf-parse';
-
-
-
-// test('Read specific text from PDF', async ({ page }) => {
-//   await page.goto('https://kmsaddam.com/resume/Updated_SQA_Saddam_2026.pdf');
-
-//   const [download] = await Promise.all([
-//     page.waitForEvent('download'),
-//     page.click('text=Download PDF')
-//   ]);
-
-//   const filePath = `downloads/${download.suggestedFilename()}`;
-//   await download.saveAs(filePath);
-
-//   const buffer = fs.readFileSync(filePath);
-//   const pdfData = await pdfParse(buffer);
-
-//   // ✅ Verify specific text
-// //   expect(pdfData.text).toContain('Invoice Number: 12345');
-//   expect(pdfData.text).toContain('dev.saddam@gmaill.com');
-
-// });
-
 import { test, expect } from '@playwright/test';
-import pdfParse from 'pdf-parse';
+
+import { PDFParse } from 'pdf-parse';
 
 test('Read PDF', async ({ request }) => {
-  const res = await request.get('https://kmsaddam.com/resume/Updated_SQA_Saddam_2026.pdf');
-  const buffer = await res.body();
+  // const res = await request.get('https://kmsaddam.com/resume/Updated_SQA_Saddam_2026.pdf');
+  // const buffer = await res.body();
 
-  const pdfData = await pdfParse(buffer);
+  // const pdfData = await pdfParse(buffer);
 
-  expect(pdfData.text).toContain('dev.saddam@gmaill.com');
+  const parser = new PDFParse({ url: 'https://kmsaddam.com/resume/Updated_SQA_Saddam_2026.pdf' });
+
+	const result = await parser.getText();
+	//console.log(result.text);
+
+  expect(result.text).toContain('dev.saddam@gmaill.com');
 });
